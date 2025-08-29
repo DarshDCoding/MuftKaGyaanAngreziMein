bb13b8ce4f583987dc8ed58e603003af6a3c4cf0
Darsh <darshdcoding@gmail.com>
2025-08-30 01:09:33 +0530

    adding typing effect

--- script.js
+++ script.js
@@ -12,22 +12,37 @@ const adviceNoElement = document.getElementById("advice-no");
 const adviceElement = document.getElementById("advice");
 const getAdviceBtn = document.getElementById("get-advice");
 
-getAdviceBtn.addEventListener('click', ()=>{
-    let newAdvice = ""
 
-    fetch('https://api.adviceslip.com/advice')
-.then(res => res.json())
-.then(data =>{
-    newAdvice = data.slip.advice
-    adviceNum = data.slip.id
-    
-    adviceNoElement.innerText = `ADVICE #${adviceNum}`;
-    adviceElement.innerText = `"${newAdvice}"`
-})
-.catch (err =>{
-    adviceNoElement.innerText = "Darsh's Advice";
-    adviceElement.innerText = '"Believe in yourself... (Try later for better advice)."'
+function typeAdviceText(text, element, speed = 30) {
+    element.innerHTML = '';
+    let i = 0;
+    function typeChar() {
+        if (i < text.length) {
+            // Use &nbsp; for spaces so they are visible during typing
+            const char = text.charAt(i) === ' ' ? '&nbsp;' : text.charAt(i);
+            element.innerHTML += char;
+            i++;
+            setTimeout(typeChar, speed);
+        } else {
+            // After typing, restore normal text for selection/copying
+            element.innerText = text;
+        }
+    }
+    typeChar();
 }
-) 
-})
+
+getAdviceBtn.addEventListener('click', () => {
+    fetch('https://api.adviceslip.com/advice')
+        .then(res => res.json())
+        .then(data => {
+            const newAdvice = data.slip.advice;
+            const adviceNum = data.slip.id;
+            adviceNoElement.innerText = `ADVICE #${adviceNum}`;
+            typeAdviceText(`"${newAdvice}"`, adviceElement);
+        })
+        .catch(err => {
+            adviceNoElement.innerText = "Darsh's Advice";
+            typeAdviceText('"Believe in yourself... (Try later for better advice)."', adviceElement);
+        });
+});
 
